7fc6b217458de75202d5883f0a8b583c7dd05539
loliconaoquadrado123 <68407584+loliconaoquadrado123@users.noreply.github.com>
2024-09-16 18:29:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from pyquery import PyQuery as pq
 import time
-import threading
 headers={
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0',
     }
